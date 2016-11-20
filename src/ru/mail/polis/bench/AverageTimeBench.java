7db4b09be5066266ce1e78cca2bb9164045be2cb
Nechaev_Mikhail <mikhail.nechaev@odnoklassniki.ru>
2016-11-20 18:50:36 +0300

    predefined array example

--- src/ru/mail/polis/bench/AverageTimeBench.java
+++ src/ru/mail/polis/bench/AverageTimeBench.java
@@ -1,16 +1,19 @@
 package ru.mail.polis.bench;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -19,29 +22,46 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 import ru.mail.polis.sort.BubbleSort;
 import ru.mail.polis.sort.Helper;
 
-@State(Scope.Thread)
+/**
+ * Created by Nechaev Mikhail
+ * Since 20/11/16.
+ */
+
+@State(Scope.Benchmark)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
-public class BubbleSortBench {
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+public class AverageTimeBench {
 
-    private int[] a;
+    int[][] data;
+    int[] curr;
+    int index;
+
+    @Setup(value = Level.Trial)
+    public void setUpTrial() {
+        data = new int[10][100];
+        for (int i = 0; i < 10; i++) {
+            //define arrays here
+            data[i] = Helper.gen(100);
+        }
+    }
 
     @Setup(value = Level.Invocation)
     public void setUpInvocation() {
-        a = Helper.gen(1000);
+        curr = Arrays.copyOf(data[index], data[index].length);
+        index = (index + 1) % 10;
     }
 
     @Benchmark
-    public void measureBubbleSort(Blackhole bh) {
-        bh.consume(BubbleSort.sort(a));
+    public void measureBubbleSort() {
+        BubbleSort.sort(curr);
     }
 
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
-                .include(BubbleSortBench.class.getSimpleName())
-                .warmupIterations(5)
-                .measurementIterations(5)
-                .forks(1)
+                .include(AverageTimeBench.class.getSimpleName())
                 .build();
 
         new Runner(opt).run();
