067c1cd2aea4aaf9c3488ddc4993ff2fcc37f2c2
techno-aads <nya.nya72@gmail.com>
2017-11-13 01:02:55 +0300

    IntKeyObject

---
+++ src/ru/mail/polis/structures/IntKeyObject.java
@@ -0,0 +1,15 @@
+package ru.mail.polis.structures;
+
+/**
+ * Created by Nechaev Mikhail
+ * Since 13/11/2017.
+ */
+public interface IntKeyObject<V> extends Comparable<IntKeyObject> {
+    int getKey();
+    V getValue();
+
+    @Override
+    default int compareTo(IntKeyObject anotherIntKeyObject) {
+        return Integer.compare(getKey(), anotherIntKeyObject.getKey());
+    }
+}
