c7d7f162a2c8a278c847e2f4bc5422716957c8df
techno-aads <nya.nya72@gmail.com>
2017-11-14 02:22:50 +0300

    SortOnComparisons

--- src/ru/mail/polis/sort/SortUtils.java
+++ src/ru/mail/polis/sort/SortUtils.java
@@ -1,5 +1,6 @@
 package ru.mail.polis.sort;
 
+import java.util.Comparator;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -29,6 +30,22 @@ public class SortUtils {
         boolean isSorted = true;
         for (int i = 0; i < a.length - 1 && isSorted; i++) {
             isSorted = a[i] <= a[i + 1];
+        }
+        return isSorted;
+    }
+
+    public static <T extends Comparable<? super T>> boolean isArraySorted(T[] array) {
+        boolean isSorted = true;
+        for (int i = 0; i < array.length - 1 && isSorted; i++) {
+            isSorted = array[i].compareTo(array[i + 1]) <= 0;
+        }
+        return isSorted;
+    }
+
+    public static <T> boolean isArraySorted(T[] array, Comparator<T> comparator) {
+        boolean isSorted = true;
+        for (int i = 0; i < array.length - 1 && isSorted; i++) {
+            isSorted = comparator.compare(array[i], array[i + 1]) <= 0;
         }
         return isSorted;
     }
